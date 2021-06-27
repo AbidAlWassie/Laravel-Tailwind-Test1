6e988d6b3e42777c515ab6c4460cf4a6b4a64dff
Abid Al-Wasi <abidalwasi@outlook.com>
2021-06-27 17:50:14 +0600

    updated project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,14 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="dark">
 
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>App Test1</title>
+  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
-<body>
+<body class="bg-gray-100 text-gray-900 dark:bg-gray-800 dark:text-gray-100">
+
+  <nav class="p-6 bg-white dark:bg-gray-900 flex justify-between mb-6">
+
+    <ul class="flex items-center">
+      <li><a href="javascript:void(0)" class="p-3">Home</a></li>
+      <li><a href="javascript:void(0)" class="p-3">Dashboard</a></li>
+      <li><a href="javascript:void(0)" class="p-3">Post</a></li>
+    </ul>
+
+    <ul class="flex items-center">
+      <li><a href="javascript:void(0)" class="p-3">Abid Al Wassie</a></li>
+      <li><a href="javascript:void(0)" class="p-3">Login</a></li>
+      <li><a href="javascript:void(0)" class="p-3">Register</a></li>
+      <li><a href="javascript:void(0)" class="p-3">Logout</a></li>
+    </ul>
+
+  </nav>
+
   @yield('content')
 </body>
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    Index
+  <div class="flex justify-center">
+    <div class="w-8/12 bg-white p-6 rounded-lg dark:bg-gray-700 dark:text-gray-100">
+    Post
+    </div>
+  </div>
 @endsection
